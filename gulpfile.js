f038d89bb54b57e552e121afcc6a3190d3552c48
Scapain <dofustouch@no-emu.com>
2017-02-17 13:48:43 +0100

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 const gulp = require('gulp');
 const del = require('del');
-const fs = require('fs');
+const async = require('async');
+const fs = require('fs-extra')
 const typescript = require('gulp-typescript');
 const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync');
 const tslint = require('gulp-tslint');
+const request = require('request');
 const reload = browserSync.reload;
+const replace = require("replace");
 
 var tsProject = typescript.createProject('tsconfig.json');
 
@@ -16,20 +19,84 @@ const paths = {
     srcTsFiles: 'src/**/*.ts',
 }
 
+function downloadAsset(cb) {
+    request.get({
+        url: 'https://proxyconnection.touch.dofus.com/assetMap.json',
+        forever: true
+    }, function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+            let bodyParse = JSON.parse(body);
+
+            async.each(bodyParse.files, function (file, callback) {
+                downloadBinary('https://proxyconnection.touch.dofus.com/' + file.filename, './game/' + file.filename, callback)
+            }, function (err) {
+
+                if (err) {
+                    console.log('A file failed to process');
+                    cb();
+                } else {
+                    console.log('All files have been processed successfully');
+                    cb();
+                }
+            });
+        } else {
+            console.error(error);
+            cb(error);
+        }
+    });
+}
+
+
+function downloadBinary(uri, savePath, callback) {
+    fs.ensureFile(savePath, function (err) {
+        request.head(uri, function (error, response, body) {
+            request(uri).pipe(fs.createWriteStream(savePath)).on('close', callback);
+        });
+    });
+}
+
+gulp.task('game', function (cb) {
+
+    async.parallel([
+        function (callback) {
+            downloadBinary('https://proxyconnection.touch.dofus.com/build/script.js', './game/script.js', callback);
+            
+        },
+        function (callback) {
+            downloadBinary('https://proxyconnection.touch.dofus.com/build/styles-native.css', './game/style-native.css', function () {
+                replace({
+                    regex: "cdvfile://localhost/persistent/data/assets",
+                    replacement: "assets",
+                    paths: ['./game/style-native.css'],
+                    recursive: true,
+                    silent: true,
+                });
+                callback();
+            });
+        },
+        function (callback) {
+            downloadAsset(cb)
+        }
+    ], function (err, result) {
+        cb();
+    });
+});
+
 // clean the contents of the distribution directory
-gulp.task('clean', function() {
+gulp.task('clean', function () {
     return del(paths.distFiles);
 });
 
 // copy static assets - i.e. non TypeScript compiled source
-gulp.task('copy:assets', ['clean'], function() {
+gulp.task('copy:assets', ['clean'], function () {
     return gulp.src([paths.srcFiles, '!' + paths.srcTsFiles])
         .pipe(gulp.dest(paths.dist))
 });
 
 
 // TypeScript compile
-gulp.task('compile', ['clean'], function() {
+gulp.task('compile', ['clean'], function () {
     // load the tsconfig each time as it changes!
     //const tscConfig = JSON.parse(fs.readFileSync('./tsconfig.json', 'UTF8'));
     /*return gulp
@@ -55,14 +122,14 @@ gulp.task('compile', ['clean'], function() {
 });
 
 // linting
-gulp.task('tslint', function() {
+gulp.task('tslint', function () {
     /*return gulp.src(paths.srcTsFiles)
         .pipe(tslint())
         .pipe(tslint.report('verbose'));*/
 });
 
 // Run browsersync for development
-gulp.task('serve', ['build'], function() {
+gulp.task('serve', ['build'], function () {
     /*browserSync({
         server: {
             baseDir: ''
