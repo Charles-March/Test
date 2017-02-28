13a6f564658102c07acdbcf73ecb8523de5eca76
Scapain <dofustouch@no-emu.com>
2017-02-28 17:59:31 +0100

    add shortcut mod

--- src/browser/app/main/game/game.component.ts
+++ src/browser/app/main/game/game.component.ts
@@ -3,7 +3,7 @@ import {
     EventEmitter
 } from '@angular/core';
 import {Tab} from './../tab/tab';
-import {ShortCuts} from './../../shortcuts/shortcuts';
+import {Shortcuts} from './shortcuts/shortcuts';
 import * as async from 'async';
 import {IpcRendererService} from './../../../shared/electron/ipcrenderer.service';
 import {TranslateService} from "ng2-translate";
@@ -37,7 +37,7 @@ export class GameComponent implements OnInit, AfterViewInit {
 
     @Input() private tab: Tab;
     @Output() selectTab = new EventEmitter();
-    private shortCuts: ShortCuts;
+    private shortcuts: Shortcuts;
     private gamePath: string;
     private gameLoaded: boolean = false;
     private backupMaxZoom: number;
@@ -64,7 +64,6 @@ export class GameComponent implements OnInit, AfterViewInit {
     ngAfterViewInit() {
         // after View Init get the iFrame
         this.tab.window = this.window['Frame' + this.tab.id].contentWindow;
-        this.shortCuts = new ShortCuts(this.tab.window);
     }
 
     private gameReady(): void {
@@ -92,6 +91,7 @@ export class GameComponent implements OnInit, AfterViewInit {
             default:
                 this.notifications.removeAllListeners();
                 this.notifications.reset();
+                this.shortcuts.reset();
         }
 
         this.setMods();
@@ -113,10 +113,12 @@ export class GameComponent implements OnInit, AfterViewInit {
                     });
                 });
                 this.notifications.on('focusTab', () => {
+
                     this.zone.run(() => {
                         this.selectTab.emit(this.tab);
                     });
                 });
+                this.shortcuts = new Shortcuts(this.tab.window, this.settingsService.option.shortcuts);
         }
     }
 
@@ -128,10 +130,7 @@ export class GameComponent implements OnInit, AfterViewInit {
             this.checkMaxZoom();
         };
 
-
-        // event -> log into the world
-        this.tab.window.gui.playerData.on("characterSelectedSuccess", () => {
-
+        let onCharacterSelectedSuccess = () =>{
             // retrieve character name and update zone.js
             this.zone.run(() => {
                 this.tab.character = this.tab.window.gui.playerData.characterBaseInformations.name;
@@ -139,34 +138,18 @@ export class GameComponent implements OnInit, AfterViewInit {
                 this.titleService.setTitle(this.tab.character);
             });
 
-            // bind event IG
-            //this.bindEventIG();
             this.checkMaxZoom();
+        };
 
-            // bind shortcut
-            this.bindShortcuts();
-        });
-
-        this.tab.window.gui.on("disconnect", () => {
-            this.unBindShortcuts();
+        let onDisconnect = () => {
             this.zone.run(() => {
                 this.tab.isLogged = false;
                 this.tab.character = null;
             });
-        });
+        }
 
-        // event -> electron ask for reload setting
-        this.ipcRendererService.on('reload-shortcuts', (event: any, arg: any) => {
-            if (this.tab.isLogged) {
-                console.log('receive->reload-shortcuts');
-
-                // unbind all registered shortcuts
-                this.unBindShortcuts();
-
-                // re-bind new shortcuts
-                this.bindShortcuts();
-            }
-        });
+        this.tab.window.gui.playerData.on("characterSelectedSuccess", onCharacterSelectedSuccess);
+        this.tab.window.gui.on("disconnect", onDisconnect);
 
         this.tab.window.isoEngine.mapScene._refreshAreasBackup = this.tab.window.isoEngine.mapScene._refreshAreas;
         this.tab.window.isoEngine.mapScene._refreshAreas = function () {
@@ -186,52 +169,5 @@ export class GameComponent implements OnInit, AfterViewInit {
             this.backupMaxZoom,
             this.backupMaxZoom + (this.tab.window.isoEngine.mapScene.canvas.height / 800 - 1)
         );
-    }
-
-
-    private unBindShortcuts(): void {
-        this.shortCuts.unBindAll();
-    }
-
-    private bindShortcuts(): void {
-
-        // end turn
-        this.shortCuts.bind(this.settingsService.option.shortcuts.diver.end_turn, () => {
-            this.tab.window.gui.fightManager.finishTurn()
-        });
-
-        // open chat
-        this.shortCuts.bind(this.settingsService.option.shortcuts.diver.open_chat, () => {
-            this.tab.window.gui.chat.activate()
-        });
-
-        // spell
-        async.forEachOf(this.settingsService.option.shortcuts.spell, (shortcut: string, index: number) => {
-            this.shortCuts.bind(shortcut, () => {
-                this.tab.window.gui.shortcutBar._panels.spell.slotList[index].tap();
-                //this.tab.window.gui.shortcutBar.panels.spell.slotList[index].tap();
-            });
-        });
-
-        // item
-        async.forEachOf(this.settingsService.option.shortcuts.item, (shortcut: string, index: number) => {
-            this.shortCuts.bind(shortcut, () => {
-                //this.tab.window.gui.shortcutBar.panels.item.slotList[index].tap();
-                this.tab.window.gui.shortcutBar._panels.item.slotList[index].tap();
-            });
-        });
-
-        // interfaces
-        async.forEachOf(this.settingsService.option.shortcuts.interface.getAll(), (inter: any) => {
-            this.tab.window.gui.menuBar._icons._childrenList.forEach((element: any, index: number) => {
-                if (element.id.toUpperCase() == inter.key.toUpperCase()) {
-                    this.shortCuts.bind(inter.value, () => {
-                        let newIndex = index;
-                        this.tab.window.gui.menuBar._icons._childrenList[newIndex].tap();
-                    });
-                    return;
-                }
-            });
-        });
     }
 }
